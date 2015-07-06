426f6c6bfefe2ef6e566ea705aa2a537dee6be52
Ryan Burke <ryanb-93@hotmail.co.uk>
2015-07-06 16:57:21 +0100

    Added code

---
+++ applefy.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from subprocess import call, Popen, PIPE
+from spotify import Link, Image
+from jukebox import Jukebox, container_loaded
+import os, sys
+import threading
+import time
+
+playback = False # set if you want to listen to the tracks that are currently ripped (start with "padsp ./jbripper.py ..." if using pulse audio)
+rawpcm = False # also saves a .pcm file with the raw PCM data as delivered by libspotify ()
+
+pcmfile = None
+pipe = None
+ripping = False
+end_of_track = threading.Event()
+
+def printstr(str): # print without newline
+    sys.stdout.write(str)
+    sys.stdout.flush()
+
+def shell(cmdline): # execute shell commands (unicode support)
+    call(cmdline, shell=True)
+
+def rip_init(session, track, playlist_name):
+    global pipe, ripping, pcmfile, rawpcm
+    num_track = "%02d" % (track.index(),)
+    mp3file = track.name()+".mp3"
+    pcmfile = track.name()+".pcm"
+
+    print("playlist name: " + playlist_name)
+    directory = os.getcwd() + "/" + playlist_name + "/"
+    print("directory name: " + directory)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    printstr("ripping " + mp3file + " ...")
+    p = Popen("lame --silent -V 0 -h -r - \""+ directory + mp3file+"\"", stdin=PIPE, shell=True)
+    pipe = p.stdin
+    if rawpcm:
+      pcmfile = open(directory + pcmfile, 'w')
+    ripping = True
+
+
+def rip_terminate(session, track):
+    global ripping, pipe, pcmfile, rawpcm
+    if pipe is not None:
+        print(' done!')
+        pipe.close()
+    if rawpcm:
+        pcmfile.close()
+    ripping = False
+
+def rip(session, frames, frame_size, num_frames, sample_type, sample_rate, channels):
+    if ripping:
+        printstr('.')
+        pipe.write(frames);
+        if rawpcm:
+          pcmfile.write(frames)
+
+def rip_id3(session, track, playlist_name): # write ID3 data
+    num_track = "%02d" % (track.index(),)
+    mp3file = track.name()+".mp3"
+    artist = track.artists()[0].name()
+    album = track.album().name()
+    title = track.name()
+    year = track.album().year()
+    directory = os.getcwd() + "/" + playlist_name + "/"
+    # write id3 data
+    cmd = "eyeD3" + \
+          " -t \"" + title + "\"" + \
+          " -a \"" + artist + "\"" + \
+          " -A \"" + album + "\"" + \
+          " -n " + str(num_track) + \
+          " -Y " + str(year) + \
+          " -Q " + \
+          " \"" + directory + mp3file + "\""
+    shell(cmd)
+
+class RipperThread(threading.Thread):
+    def __init__(self, ripper):
+        threading.Thread.__init__(self)
+        self.ripper = ripper
+
+    def run(self):
+        # wait for container
+        container_loaded.wait()
+        container_loaded.clear()
+
+        # create track iterator
+        link = Link.from_string(sys.argv[3])
+        if link.type() == Link.LINK_PLAYLIST:
+            playlist = link.as_playlist()
+            print('Loading the playlist: ' + str(playlist))
+            while not playlist.is_loaded():
+                time.sleep(0.1)
+            print('done')
+            itrack = iter(playlist)
+
+        # ripping loop
+        session = self.ripper.session
+        for track in itrack:
+                self.ripper.load_track(track)
+                rip_init(session, track, str(playlist))
+                self.ripper.play()
+                time.sleep(1)
+                end_of_track.clear() # TODO check if necessary
+                rip_terminate(session, track)
+                rip_id3(session, track, str(playlist))
+
+        self.ripper.disconnect()
+
+class Ripper(Jukebox):
+    def __init__(self, *a, **kw):
+        Jukebox.__init__(self, *a, **kw)
+        self.ui = RipperThread(self) # replace JukeboxUI
+        self.session.set_preferred_bitrate(1) # 320 bps
+
+    def music_delivery_safe(self, session, frames, frame_size, num_frames, sample_type, sample_rate, channels):
+        rip(session, frames, frame_size, num_frames, sample_type, sample_rate, channels)
+        if playback:
+            return Jukebox.music_delivery_safe(self, session, frames, frame_size, num_frames, sample_type, sample_rate, channels)
+        else:
+            return num_frames
+
+    def end_of_track(self, session):
+        Jukebox.end_of_track(self, session)
+        end_of_track.set()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+		ripper = Ripper(sys.argv[1],sys.argv[2]) # login
+		ripper.connect()
+    else:
+		print "usage : \n"
+		print "	  ./applefy.py [username] [password] [spotify_playlist_url]"
+		print "example : \n"
+		print "   ./applefy.py user pass spotify:user:username:playlist:4vkGNcsS8lRXj4q945NIA4 0 - clones entire playlist"
